42342c9eda36d0722f686b9d8fc0be5fd8f548a1
clebertmarctyson <marctysonclebert9@gmail.com>
2022-09-17 03:56:42 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const qrcode = require("qrcode");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("/public"));
